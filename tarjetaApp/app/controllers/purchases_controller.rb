be81fd2ec09649b9ab53df08ff80c9fb7ae2de61
Carlos Saito <c.saito@alumnos.upm.es>
2016-11-11 13:00:26 +0100

    Add Purchase controller and views

--- tarjetaApp/app/controllers/purchases_controller.rb
+++ tarjetaApp/app/controllers/purchases_controller.rb
@@ -1,2 +1,28 @@
 class PurchasesController < ApplicationController
+  def index
+    @purchases = Purchase.all
+  end
+
+  def show
+    @purchases = Purchase.find(params[:id])
+  end
+
+  def new
+    @purchase = Purchase.new
+  end
+
+  def create
+    @purchase = Purchase.new(purchase_params)
+
+    if @purchase.save
+      render 'index'
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def purchase_params
+    params.require(:purchase).permit(:id_store, :buy_date, :total, :card_id)
+  end
 end
