b9b3791e9b5717fe91b0ac2d30b65422ef243ab6
Carlos Saito <c.saito@alumnos.upm.es>
2016-11-11 16:46:39 +0100

    Fix. "set_default_values" to creation, not init

--- tarjetaApp/app/models/card.rb
+++ tarjetaApp/app/models/card.rb
@@ -3,7 +3,7 @@ class Card < ActiveRecord::Base
   has_many :purchases
 
   # Default values
-  after_initialize :set_default_values
+  before_create :set_default_values
 
   def set_default_values
     self.points = 0
