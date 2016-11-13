4b8c15727591efce19585f22cc231690dfb4aea8
Carlos Saito <c.saito@alumnos.upm.es>
2016-11-13 18:09:15 +0100

    Add Card views: show and edit

--- tarjetaApp/app/controllers/cards_controller.rb
+++ tarjetaApp/app/controllers/cards_controller.rb
@@ -1,13 +1,30 @@
 class CardsController < ApplicationController
   def index
-    @cards = Card.all 
+    if params[:client_id]
+      @cards = Card.where(client_id: params[:client_id])
+    else
+      @cards = Card.all
+    end
   end
+
   def show
-    @card = Card.find(params[:id])
+    if params[:client_id]
+      @card = Card.where(client_id: params[:client_id]).find(params[:id])
+    else
+      @card = Card.find(params[:id])
+    end
   end
 
   def new
-    @card = Card.new
+    if params[:client_id]
+      @card = Card.where(client_id: params[:client_id]).new
+    else
+      @card = Card.new
+    end
+  end
+
+  def edit
+    @card = Card.find(params[:id])
   end
 
   def create
@@ -16,10 +33,25 @@ class CardsController < ApplicationController
     redirect_to client_path(@client)
   end
 
+  def update
+    @card = Card.find(params[:id])
+
+    if @card.update(card_edit_params)
+      redirect_to @card
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
     @card = Card.find(params[:id])
     @card.destroy
 
     redirect_to @card
+  end
+
+  private
+  def card_edit_params
+    params.require(:card).permit(:id, :points, :status)
   end
 end
