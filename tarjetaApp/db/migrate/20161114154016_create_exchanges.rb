ee164f6a4f7c2baa2e73db8ca40b8d64081bd23d
Daniel Melero Chaves <daniel.mchaves@alumnos.upm.es>
2016-11-14 16:46:11 +0100

    Mirror fixing 3

--- tarjetaApp/db/migrate/20161114154016_create_exchanges.rb
+++ tarjetaApp/db/migrate/20161114154016_create_exchanges.rb
@@ -1,8 +1,8 @@
 class CreateExchanges < ActiveRecord::Migration
   def change
     create_table :exchanges do |t|
-      t.int :id_card
-      t.int :id_price
+      t.references :card, index: true, foreign_key: true
+      t.references :price, index: true, foreign_key: true
       t.timestamp :exchange_date
 
       t.timestamps null: false
