05530bd2e7796b5efae030966342a81f862a84f8
Daniel Melero Chaves <daniel.mchaves@alumnos.upm.es>
2016-11-14 16:30:34 +0100

    Price controler

---
+++ tarjetaApp/app/controllers/prices_controller.rb
@@ -0,0 +1,45 @@
+class PricesController < ApplicationController
+  def index
+    @prices = Price.all?
+  end
+
+  def show
+    @prices = Price.find(params[:id])
+  end
+
+  def new
+    @prices = Price.new
+  end
+
+  def edit
+    @prices = Price.find(params[:id])
+  end
+
+  def create
+    render 'new'
+  end
+
+  def update
+    @price = Price.find(params[:id])
+
+    if @price.update(purchase_params)
+      redirect_to @price
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @price = Price.find(params[:id])
+    @price.destroy
+
+    redirect_to @price
+  end
+
+  private
+  def purchase_params
+    params.require(:price).permit(:id, :store_id, :buy_date, :total, :card)
+  end
+
+
+end
