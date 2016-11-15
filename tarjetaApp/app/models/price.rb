d43df3e47abe910db49644b1b09f950f411dd7a4
Daniel Melero Chaves <daniel.mchaves@alumnos.upm.es>
2016-11-15 11:27:12 +0100

    Price model

--- tarjetaApp/app/models/price.rb
+++ tarjetaApp/app/models/price.rb
@@ -1,3 +1,35 @@
 class Price < ActiveRecord::Base
-  
+
+   # Default values
+   before_create :set_default_values
+
+   def change_status(status)
+     self.status = 0
+   end
+
+   def change_requirement(requirement)
+     self.requirement = requirement
+   end
+
+   def increase_lot
+     self.lot += 1
+
+     if self.lot == 1
+       change_status(true)
+     end
+   end
+
+   def decrease_lot
+     self.lot -= 1
+
+     if self.lot == 0
+       change_status(false)
+     end
+   end
+
+   def set_default_values
+     self.status = false
+     self.requirement = 0
+     self.lot = 0
+   end
 end
