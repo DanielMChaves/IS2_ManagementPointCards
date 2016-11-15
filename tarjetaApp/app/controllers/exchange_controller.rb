6f3d576d75329c1c1794086951bc3e2861eadfdb
Daniel Melero Chaves <daniel.mchaves@alumnos.upm.es>
2016-11-15 17:53:31 +0100

    Base of Exchange, it need testing

--- tarjetaApp/app/controllers/exchange_controller.rb
+++ tarjetaApp/app/controllers/exchange_controller.rb
@@ -1,2 +1,50 @@
-class ExchangeController < ApplicationController
+class ExchangesController < ApplicationController
+  def index
+    @exchange = Exchange.all
+  end
+
+  def show
+    @exchange = Exchange.find(params[:id])
+  end
+
+  def new
+    @exchange = Exchange.new
+  end
+
+  def edit
+    @exchange = Exchange.find(params[:id])
+  end
+
+  def create
+    @exchange = Exchange.new(exchange_params)
+
+    if @exchange.save
+      redirect_to @exchange
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @exchange = Exchange.find(params[:id])
+
+    if @exchange.update(exchange_params)
+      redirect_to @exchange
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @exchange = Exchange.find(params[:id])
+    @exchange.destroy
+
+    redirect_to @exchange
+  end
+
+  private
+  def exchange_params
+    params.require(:exchange).permit(:card_id, :price_id, :exchange_date)
+  end
+
 end
--- tarjetaApp/config/routes.rb
+++ tarjetaApp/config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   resources :cards, only: [:index]
   resources :purchases, only: [:index]
   resources :prices
+  resources :exchanges
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
