32a670bd47ad49472aac4bcb507d5097b613489b
Carlos Saito <c.saito@alumnos.upm.es>
2016-11-21 11:32:32 +0100

    Dummy data. SEE IMPORTANT NOTES BELOW
    Para usar estos datos, ejecutar el comando:
    
    ```
    rake db:setup
    ```
    
    Opcionalmente, se puede eliminar todo el esquema e incluir estos datos
    usando:
    
    ```
    rake db:drop db:create db:migrate db:setup
    ```

--- tarjetaApp/db/seeds.rb
+++ tarjetaApp/db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create 3 clients
+edmundo = Client.create(
+  id: '1234567B',
+  name: 'Alfredo',
+  surname: 'Ravot Clavo',
+  tlf: '+34666666666',
+  email: 'etc@upm.es',
+  address: 'Campus Montegancedo'
+)
+
+josefa = Client.create(
+  id: '2345678A',
+  name: 'Josefa',
+  surname: 'Iglesias',
+  tlf: '+34672890192',
+  email: 'josefa_iglesias@gmail.com',
+  address: 'Plaza de los Comendadores 32'
+)
+
+Client.create(
+  id: '00000000X',
+  name: 'None',
+  surname: 'Nothing',
+  tlf: '+34000000000',
+  email: 'example@domain.com',
+  address: 'Calle alguna, 0'
+)
+
+# Create 4 cards
+Card.create(client: edmundo)
+Card.create(client: josefa)
+Card.create(client: edmundo)
+Card.create(client: edmundo)
